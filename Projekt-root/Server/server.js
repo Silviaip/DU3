181f31661ac1724f3568b5372577e5b379eb1496
Silviaip <143509374+Silviaip@users.noreply.github.com>
2025-05-16 12:09:21 +0200

    kod

--- Projekt-root/Server/server.js
+++ Projekt-root/Server/server.js
@@ -1,8 +1,30 @@
-import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
-import { handleApiRequest } from "../../Server/api.js";
+import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 
-console.log("✅ Servern körs på http://localhost:8000");
+const url = "https://www.themealdb.com/api/json/v1/1/random.php";
+const templatePath = "./template.html";
+
+async function fetchRandomMeal() {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.meals[0];
+}
+
+function fillTemplate(template, meal) {
+  return template
+    .replaceAll("{{mealName}}", meal.strMeal)
+    .replaceAll("{{mealThumb}}", meal.strMealThumb)
+    .replaceAll("{{mealCategory}}", meal.strCategory)
+    .replaceAll("{{mealArea}}", meal.strArea)
+    .replaceAll("{{mealSource}}", meal.strSource || "#");
+}
 
 serve(async (req) => {
-  return await handleApiRequest(req);
-});
+  const meal = await fetchRandomMeal();
+
+  const template = await Deno.readTextFile(templatePath);
+  const html = fillTemplate(template, meal);
+
+  return new Response(html, {
+    headers: { "content-type": "text/html; charset=utf-8" },
+  });
+}, { port: 8080 });
