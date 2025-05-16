c342d22adf113b94cf34285f3edb308501cd1438
Silviaip <143509374+Silviaip@users.noreply.github.com>
2025-05-16 23:54:53 +0200

    drinks o meals fungerar nu

--- projekt-du3/meals.js
+++ projekt-du3/meals.js
@@ -1,7 +1,8 @@
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
+
 const RANDOM_URL = "https://www.themealdb.com/api/json/v1/1/random.php";
 const LOOKUP_URL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
-const templatePath = "./server/index.html";
+const RANDOM_DRINK_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
 async function getRandomMealDetails() {
   const res = await fetch(RANDOM_URL);
@@ -13,9 +14,16 @@ async function getRandomMealDetails() {
   return detailData.meals[0];
 }
 
+async function getRandomDrink() {
+  const res = await fetch(RANDOM_DRINK_URL);
+  const data = await res.json();
+  return data.drinks[0];
+}
+
 serve(async (req) => {
   const { pathname } = new URL(req.url);
 
+  
   if (pathname === "/meal") {
     const meal = await getRandomMealDetails();
     return new Response(JSON.stringify(meal), {
@@ -23,17 +31,36 @@ serve(async (req) => {
     });
   }
 
- if (pathname === "/") {
-  const html = await Deno.readTextFile("./server/index.html");
-  return new Response(html, {
-    headers: { "Content-Type": "text/html; charset=utf-8" }, // ✅
-  });
-}
+  if (pathname === "/drink") {
+    const drink = await getRandomDrink();
+    return new Response(JSON.stringify(drink), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 
+  if (pathname === "/") {
+    const html = await Deno.readTextFile("./server/index.html");
+    return new Response(html, {
+      headers: { "Content-Type": "text/html; charset=utf-8" },
+    });
+  }
 
-  // Statisk filserver för JS
+  if (pathname === "/style.css") {
+    const css = await Deno.readTextFile("./client/style.css");
+    return new Response(css, {
+      headers: { "Content-Type": "text/css" },
+    });
+  }
+
   if (pathname === "/mealsfetch.js") {
     const js = await Deno.readTextFile("./server/mealsfetch.js");
+    return new Response(js, {
+      headers: { "Content-Type": "application/javascript" },
+    });
+  }
+
+  if (pathname === "/drinks.js") {
+    const js = await Deno.readTextFile("./server/drinks.js");
     return new Response(js, {
       headers: { "Content-Type": "application/javascript" },
     });
