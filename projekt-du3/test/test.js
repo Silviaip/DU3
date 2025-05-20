3b607172b4e42ed3dd18307c7938fb8151fc3065
Maja Dahlberg <maja04dahlberg@gmail.com>
2025-05-20 12:27:44 +0200

    Påbörjat test-sida

---
+++ projekt-du3/test/test.js
@@ -0,0 +1,5 @@
+// man testar skicka requests till vår server med olika endpoints, så ska vi hantera svaret man får tillbaks beroende på vad vi får tillbaka som response
+
+// skicka några dåliga requests för att se hur servern hanterar det
+
+// 
